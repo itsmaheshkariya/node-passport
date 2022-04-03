4e2ba1d5a37dd954cf38ea08932ab194b6f75baa
Mahesh Kariya <itsmaheshkariya@gmail.com>
2022-04-04 02:32:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express();
 app.set('port', 3001 || process.env.PORT )
 app.get('/',(req,res)=>{
-    res.send("heythere")
+    res.send("heythere1")
 })
 app.listen(app.get('port'))
